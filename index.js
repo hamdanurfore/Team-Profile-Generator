5bee18b048bfa5c01e618b70d06e26e7aa6b68dc
Hamda <nurfore14@gmail.com>
2024-01-05 01:33:51 +0000

    intern prompts

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 const { choices } = require("yargs");
-// const template = render(teamMembers);
-// fs.writeFile("index.html", template, (err) => {
-// if (err) {
-//     throw err
-// }
-// console.log( "file has been succesfully created");
-// }) 
+function renderHTML(teamMembers) {
+    const html = render(teamMembers);
+    return html;
+}
+function saveHTMLToFile(html) {
+    fs.writeFile(outputPath, html, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("HTML file has been successfully created: " + outputPath);
+    });
+}
 
 // Teamm members array 
 const teamMembers = [];
@@ -98,7 +103,7 @@ function menu() {
         choices: [
             "Intern",
             "Engineer",
-            "Exit", 
+            "Done", 
 
         ]
     })
@@ -107,9 +112,9 @@ function menu() {
         if (choicesInput.choice === "Engineer") {
             promptEngineer();
         } else if (choicesInput.choice === "Intern") {
-            
+            promptIntern();
         } else if (choicesInput.choice === "Exit") {
-            console.log("Exiting program. Goodbye!");
+            console.log("Your team is now complete!");
         } else {
             
             console.log("Invalid choice. Please try again.");
@@ -172,4 +177,53 @@ function promptEngineer() {
         });
 }
 
+// intern prompts
+function promptIntern() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Enter the intern's name:",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Enter the intern's id:",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log("Please enter the intern's ID!")
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter the intern's email:",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log('Please enter an email!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Enter the school:",
+        },
+    ])
+        .then((internInput) => {
+            const { name, id, email, school } = internInput;
+            const intern = new Intern(name, id, email, school);
 
+            teamMembers.push(intern);
+            console.log(intern);
+
+        });
+}
